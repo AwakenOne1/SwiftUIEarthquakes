d72774a99dc51e0a176ba6c4fff90901bef9fac4
AwakenOne1 <alekseydubovik2003@icloud.com>
2024-02-13 16:17:45 +0300

    Initial Commit

---
+++ SwiftUIEarthquakes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIEarthquakes
+//
+//  Created by Alexey Dubovik on 13.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIEarthquakes/SwiftUIEarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEarthquakesApp.swift
+//  SwiftUIEarthquakes
+//
+//  Created by Alexey Dubovik on 13.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
